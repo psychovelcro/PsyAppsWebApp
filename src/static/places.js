785b0a29e954731b240de2a29329810b4fc3c6d4
psychovelcro <juanreyfotos@gmail.com>
2021-11-04 16:24:22 +0100

    correccion

--- src/static/places.js
+++ src/static/places.js
@@ -9,7 +9,7 @@ const places = [
   {
     title: 'Android & iOS Apps',
     description:
-      'Creamos aplicaciones para iOS y Android con la última tecnología. Te asesoramos para obtener una UI increíble para tu negocio o Empresa. Nos avalas años de experiencia en la creación de Apps para diferentes sectores.',
+      'Creamos aplicaciones para iOS y Android con la última tecnología. Te asesoramos para obtener una UI increíble para tu negocio o Empresa. Nos avalan años de experiencia en la creación de Apps para diferentes sectores.',
     imageUrl: process.env.PUBLIC_URL + '/assets/apps2.jpg',
     time: 1500,
   },
