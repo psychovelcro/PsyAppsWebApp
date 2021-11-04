2106d35999bc8a12175ef7beeced7114387fa506
psychovelcro <juanreyfotos@gmail.com>
2021-11-04 13:05:32 +0100

    favicon

--- src/static/places.js
+++ src/static/places.js
@@ -2,7 +2,7 @@ const places = [
   {
     title: 'Software & Databases',
     description:
-      "Creamos aplicaciones para iOS y Android con la última tecnología. Te asesoramos para obtener una UI increíble para tu negocio o Empresa. Nos avalas años de experiencia en la creación de Apps para diferentes sectores.",
+      "Realizamos software de gestión empresrial, construimos tu base de datos o la migramos aprovechando la última técnologia para ello.",
     imageUrl: process.env.PUBLIC_URL + '/assets/soft2.jpg',
     time: 1500,
   },
